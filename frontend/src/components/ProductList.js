375bceb989f2307f7beee354b4224c2a8664a572
Kaanu0824 <Kaanu737@gmail.com>
2024-02-23 13:49:10 +0530

    Product List Add Sample

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,5 +1,4 @@
-// src/components/ProductList.js
-import { Card, CardContent, Container, Grid, Typography } from '@mui/material';
+import { Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
@@ -13,34 +12,32 @@ const ProductList = () => {
       .catch(error => console.error(error));
   }, []);
 
-  console.log(products,"show products")
-
   return (
     <Container>
-      <Grid container spacing={3}>
-        {products.map(product => (
-          <Grid item key={product._id} xs={12} sm={6} md={4}>
-            <Card>
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  {product.productName}
-                </Typography>
-                <Typography color="text.secondary">
-                  SKU: {product.sku}
-                </Typography>
-                <Typography color="text.secondary">
-                  SKU: {product.sku}
-                </Typography>
-
-                <Typography color="text.secondary">
-                productDescription: {product.productDescription}
-                </Typography>
-               
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+      <Typography variant="h4" gutterBottom>
+        Product List
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Product Name</TableCell>
+              <TableCell>SKU</TableCell>
+              <TableCell>Product Description</TableCell>
+              <TableCell>Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map(product => (
+              <TableRow key={product._id}>
+                <TableCell>{product.productName}</TableCell>
+                <TableCell>{product.sku}</TableCell>
+                <TableCell>{product.productDescription}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 };
